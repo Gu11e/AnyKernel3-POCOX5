5bd2336d659b2bf69ef03c5de449978fd5398e30
Gu11e <guillegomser@gmail.com>
2026-08-21 05:12:36 +0200

    Update kernel string placeholder in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -3,7 +3,7 @@
 
 ### AnyKernel setup
 properties() { '
-kernel.string=Kernel moonstone with KernelSU-Next by Gu11e
+kernel.string=CUSTOM_KERNEL_STRING_PLACEHOLDER
 do.devicecheck=1
 do.modules=0
 do.systemless=1
