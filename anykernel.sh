9b3b5bfb1919c3de2137790ddf29d7ea89758f9b
Gu11e <guillegomser@gmail.com>
2026-08-21 23:03:40 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,12 +4,13 @@
 ### AnyKernel setup
 properties() { '
 kernel.string=CUSTOM_KERNEL_STRING_PLACEHOLDER
+device.name1=moonstone
+device.name2=gemstone
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=moonstone
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
